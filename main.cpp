2927eb5da8d4429e2fe79245cf3e6e5f35190be6
Rodrigo Demetrio <rodrigo.demetrio.palma@gmail.com>
2018-11-25 22:45:38 -0200

    first step done - Binary Hair Matrix

--- main.cpp
+++ main.cpp
@@ -6,33 +6,44 @@
 #include <list>
 #include <cmath>
 #include <vector>
+#define T 2
  
 using namespace cv;
 using namespace std;
 
 int main()
 {
-    Mat mat_colored, mat_closed0,mat_closed45,mat_closed90,mat_closed; //Declaração da matriz da imagem colorida
+    Mat mat_colored; //Declaração da matriz da imagem colorida
+    Mat mat_hair[3], mat_closed0[3],mat_closed45[3],mat_closed90[3],mat_closed[3]; // Matrizes auxiliares para o fechamento e para os cabelos
     Mat kernel0,kernel45,kernel90; //Declaração dos kernels para fazer closing
-    mat_colored = imread("./fig1.png",CV_LOAD_IMAGE_COLOR); //carrega arquivo
-    
-    mat_closed0 = Mat::zeros(mat_colored.rows,mat_colored.cols, CV_8UC3);
-    mat_closed45 = Mat::zeros(mat_colored.rows,mat_colored.cols, CV_8UC3);
-    mat_closed90 = Mat::zeros(mat_colored.rows,mat_colored.cols, CV_8UC3);
-    mat_closed = Mat::zeros(mat_colored.rows,mat_colored.cols, CV_8UC3);
+    Mat channels[3]; // Matriz dos canais BRG separados
+    Mat generalized_grayscale[3], mat_hair_final;// Matrizes necessarias para calculo da matriz de cabelo final binaria
 
+    mat_colored = imread("./fig1.png",CV_LOAD_IMAGE_COLOR); //carrega arquivo
+
+    split(mat_colored,channels); // Separa a matriz colorida em 3 canais BGR
+
+    // Inicializa matrizes com 0
+    for (int i =0;i<3;i++){
+        mat_closed0[i] = Mat::zeros(mat_colored.rows,mat_colored.cols, CV_8UC1); // Vetor de 3 matrizes (BGR) para o grau 0
+        mat_closed45[i] = Mat::zeros(mat_colored.rows,mat_colored.cols, CV_8UC1);// Vetor de 3 matrizes (BGR) para o grau 45
+        mat_closed90[i] = Mat::zeros(mat_colored.rows,mat_colored.cols, CV_8UC1);// Vetor de 3 matrizes (BGR) para o grau 90
+        mat_closed[i] = Mat::zeros(mat_colored.rows,mat_colored.cols, CV_8UC1); // Vetor de 3 matrizes (BGR) para resultado final dos fechamentos
+        mat_hair[i] = Mat::zeros(mat_colored.rows,mat_colored.cols, CV_8UC1); // Vetor de 3 matrizes (BGR) para os cabelos
+    }
+    
     //cria os kernels para fazer o fechamento
     kernel0 = Mat::zeros(1,13, CV_8UC1);
     kernel45 = Mat::zeros(9,9, CV_8UC1);
     kernel90 = Mat::zeros(13,1, CV_8UC1);
 
-    
     // preenche os kernels com 1's
     for(int i=1;i<12;i++){
         kernel0.at<uchar>(0, i, 0) = 1;
         kernel90.at<uchar>(i, 0, 0) = 1;
     }
 
+    // preenche os kernels com 1's
     for(int j=1;j<8;j++){
         for(int k=1;k<8;k++){
             if (j == k){
@@ -41,67 +52,98 @@ int main()
         }
     }
 
-    // cout << "M = " << endl << " " << kernel135 << endl << endl;
-    // cout << "M = " << endl << " " << kernel0 << endl << endl;
-    // cout << "M = " << endl << " " << kernel45 << endl << endl;
-    // cout << "M = " << endl << " " << kernel90 << endl << endl;
     
-    // Aplica o fechamento com os 4 kernels
-    morphologyEx(mat_colored, mat_closed0,cv::MORPH_CLOSE, kernel0);
-    morphologyEx(mat_colored, mat_closed45,cv::MORPH_CLOSE, kernel45);
-    morphologyEx(mat_colored, mat_closed90,cv::MORPH_CLOSE, kernel90);
+    // Aplica o fechamento com os 3 kernels
 
+    //Blue
+    morphologyEx(channels[0], mat_closed0[0],cv::MORPH_CLOSE, kernel0);
+    morphologyEx(channels[0], mat_closed45[0],cv::MORPH_CLOSE, kernel45);
+    morphologyEx(channels[0], mat_closed90[0],cv::MORPH_CLOSE, kernel90);
+    //Red
+    morphologyEx(channels[1], mat_closed0[1],cv::MORPH_CLOSE, kernel0);
+    morphologyEx(channels[1], mat_closed45[1],cv::MORPH_CLOSE, kernel45);
+    morphologyEx(channels[1], mat_closed90[1],cv::MORPH_CLOSE, kernel90);
+    //Green
+    morphologyEx(channels[2], mat_closed0[2],cv::MORPH_CLOSE, kernel0);
+    morphologyEx(channels[2], mat_closed45[2],cv::MORPH_CLOSE, kernel45);
+    morphologyEx(channels[2], mat_closed90[2],cv::MORPH_CLOSE, kernel90);
+
+    
+    // For para calcular o maximo entre os fechamentos aplicados com 3 graus diferentes e para os 3 planos BGR
     for (int i=0; i< mat_colored.rows; i++){
-        for (int j=0 ;j< mat_colored.cols ; j++){  
-            for (int k=0 ;k< 3 ; k++){  
-                mat_closed.at<Vec3b>(i,j)[k] = max(max(mat_closed0.at<Vec3b>(i, j) [k], mat_closed45.at<Vec3b>(i, j)[k]), mat_closed90.at<Vec3b>(i, j) [k]);
+        for (int j=0 ;j< mat_colored.cols ; j++){ 
+            mat_closed[0].at<uchar>(i,j) = max(max(mat_closed0[0].at<uchar>(i,j), mat_closed45[0].at<uchar>(i,j)), mat_closed90[0].at<uchar>(i,j));
+            mat_closed[1].at<uchar>(i,j) = max(max(mat_closed0[1].at<uchar>(i,j), mat_closed45[1].at<uchar>(i,j)), mat_closed90[1].at<uchar>(i,j));
+            mat_closed[2].at<uchar>(i,j) = max(max(mat_closed0[2].at<uchar>(i,j), mat_closed45[2].at<uchar>(i,j)), mat_closed90[2].at<uchar>(i,j));
+        }
+    }
+
+    // Calcula a diferença absoluta dos canais originais (channel) e dos canais com fechamento calculados no for acima (generalized_grayscale)
+    absdiff(channels[0], mat_closed[0], generalized_grayscale[0]);
+    absdiff(channels[1], mat_closed[1], generalized_grayscale[1]);
+    absdiff(channels[2], mat_closed[2], generalized_grayscale[2]);
+
+
+    // For para construir a mat_hair, que é binaria e definida pelo treshhold T
+    for (int i=0; i< mat_colored.rows; i++){
+        for (int j=0 ;j< mat_colored.cols ; j++){ 
+            if(generalized_grayscale[0].at<uchar>(i,j) > T){
+                mat_hair[0].at<uchar>(i,j) = 0;
+            }
+            else{
+                mat_hair[0].at<uchar>(i,j) = 255;
+            }
+            if(generalized_grayscale[1].at<uchar>(i,j) > T){
+                mat_hair[1].at<uchar>(i,j) = 0;
+            }
+            else{
+                mat_hair[1].at<uchar>(i,j) = 255;
+            }
+            if(generalized_grayscale[2].at<uchar>(i,j) > T){
+                mat_hair[2].at<uchar>(i,j) = 0;
+            }
+            else{
+                mat_hair[2].at<uchar>(i,j) = 255;
+            }
+        }
+    }
+
+    // Ignora isso por enquanto
+    // merge(mat_closed,3,mat_out);
+
+    // União das 3 matrizes de cabelo B, G, R
+    mat_hair_final = mat_hair[0] + mat_hair[1] + mat_hair[2];
+    
+    // Negativo da matriz de cabelos para que os pelos fiquem branco
+    for (int i=0; i< mat_colored.rows; i++){
+        for (int j=0 ;j< mat_colored.cols ; j++){ 
+            if(mat_hair_final.at<uchar>(i,j) == 255){
+                mat_hair_final.at<uchar>(i,j) = 0;
+            }
+            else{
+                mat_hair_final.at<uchar>(i,j) = 255;
             }
         }
     }
 
     //mostra imagem com pelos e as imagens com closing
-    imshow("fechamento - kernel 0", mat_closed0);
-    imshow("fechamento - kernel 45", mat_closed45);
-    imshow("fechamento - kernel 90", mat_closed90);
-    imshow("Imagem Final", mat_closed);
+    // imshow("fechamento - kernel 0", mat_closed0);
+    // imshow("fechamento - kernel 45", mat_closed45);
+    // imshow("fechamento - kernel 90", mat_closed90);
+    // imshow("generalized_gray Blue", generalized_grayscale[0]);
+    // imshow("generalized_gray Green", generalized_grayscale[0]);
+    // imshow("generalized_gray Red", generalized_grayscale[0]);
+    // imshow("Blue", mat_closed[0]);
+    // imshow("Green", mat_closed[0]);
+    // imshow("Red", mat_closed[0]);
+    // imshow("Juntos BGR", mat_out);
+    // imshow("Cabelo B", mat_hair[0]);
+    // imshow("Cabelo G", mat_hair[1]);
+    // imshow("Cabelo R", mat_hair[2]);
+    imshow("Matriz Cabelos", mat_hair_final);
     imshow("Imagem Original", mat_colored);
     waitKey(0);
     destroyAllWindows();
-
-
-    // //lista de pontos do trajeto
-    // list<Point2d> path;
-
-    // //Point pega (x,y) e não (linha,coluna), o canto superior esquerdo é (0,0) 
-    // Point2d start = Point(415,260);
-    // Point2d end = Point(1000,815);
-    
-    // //instancia do editor que faz transformações na imagem
-    // HairRemoval myEditor;
-    
-    // //As matrizes foram criadas?
-    // if( !mat_colored.data || !mat_grayscale.data || !mat_hist.data) {
-    //     cout << "mat invalido!" << endl;
-    //     return -1;
-    // }
-    
-    // myEditor.toGrayscale(&mat_colored,&mat_grayscale); //RGB -> Grayscale
-
-
-    // cout << "Distancia em linha reta (em pixels) = " << myEditor.euclidianDistance(path) << endl;
-    
-    // //cria os arquivos
-    // imwrite("mars_path.bmp",mat_colored);
-    // imwrite("mars_gray.bmp",mat_grayscale);
-    // imwrite("mars_hist.bmp",mat_hist);
-    
-    // //mostra janela
-    // namedWindow("Colored_Path",CV_WINDOW_AUTOSIZE); 
-    // imshow("Colored_Path", mat_colored);
-     
- 
-    // waitKey(0);
-    // destroyAllWindows();
 
     return 0;
 }
