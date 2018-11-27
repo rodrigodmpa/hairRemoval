4ba77e01b68edda4a4069c2fff8d40307abbb4fa
Paulo <paulo_btn@yahoo.com.br>
2018-11-27 09:55:38 -0200

    tentativa de implementar interpolação (nao 100%)

--- main.cpp
+++ main.cpp
@@ -3,11 +3,12 @@
 #include <opencv2/opencv.hpp>
 #include "HairRemoval.hpp"
 #include <iostream>
+#include <math.h>
 #include <list>
 #include <cmath>
 #include <vector>
 #include <algorithm>
-#define T 22
+#define T 47
  
 using namespace cv;
 using namespace std;
@@ -28,6 +29,50 @@ void iniciaLinhas(struct lines *linhas){
     }
 }
 
+bool medianFiltering(Mat *in, Mat *out,Mat *hair, int kernel){
+
+    //argumentos devem conter dados
+    if( !in->data || !out->data ) {
+        printf("Entrada do filtro de mediana invalida!\n");
+        return false;
+    }
+
+    if(kernel %2 == 0){
+        printf("Tamanho da janela deve ser impar.\n");
+        return false;
+    }
+
+    int borderSize = (kernel-1)/2;
+    vector<uchar> kernelValues;
+
+    Mat buffer;
+    in->copyTo(buffer);
+
+    copyMakeBorder(buffer,buffer,borderSize,borderSize,borderSize,borderSize,BORDER_DEFAULT); //espelha a borda
+    
+    //passa o kernel na imagem, e o centro passa a ser a mediana. (uso a função sort)
+    for(int x = borderSize ; x < in->cols ; x++){
+        for(int y = borderSize ; y < in->rows ; y++){
+            
+            for(int kernelX = x-borderSize ; kernelX <= x+borderSize ; kernelX++ ){
+                for(int kernelY = y - borderSize ; kernelY <= y+borderSize ; kernelY++){
+                    kernelValues.push_back(buffer.at<uchar>(Point(kernelX,kernelY)));
+                }
+            }
+            sort(kernelValues.begin(), kernelValues.end());
+
+            if(hair->at<uchar>(Point(x,y)) == 255){ // Só faz a mediana se for região de cabelo
+                out->at<uchar>(Point(x,y)) = kernelValues[(kernel*kernel -1)/2];
+            }
+            kernelValues.clear();
+
+
+        }
+    }
+    
+    return true;
+}
+
 int main(int argc , char *argv[])
 {
 //    if(argc < 3){
@@ -37,13 +82,14 @@ int main(int argc , char *argv[])
     Mat mat_hair[3], mat_closed0[3],mat_closed45[3],mat_closed90[3],mat_closed[3]; // Matrizes auxiliares para o fechamento e para os cabelos
     Mat kernel0,kernel45,kernel90; //Declaração dos kernels para fazer closing
     Mat channels[3]; // Matriz dos canais BRG separados
-    Mat generalized_grayscale[3], mat_hair_final;// Matrizes necessarias para calculo da matriz de cabelo final binaria
+    Mat generalized_grayscale[3], mat_hair_final,mat_hair_final_grossa,mat_mediana;// Matrizes necessarias para calculo da matriz de cabelo final binaria
 
-    mat_colored = imread("./fig5.png",CV_LOAD_IMAGE_COLOR); //carrega arquivo
+    mat_colored = imread("./fig1.png",CV_LOAD_IMAGE_COLOR); //carrega arquivo
 
     split(mat_colored,channels); // Separa a matriz colorida em 3 canais BGR
 
     // Inicializa matrizes com 0
+    mat_mediana = Mat::zeros(mat_colored.rows,mat_colored.cols, CV_8UC1); // Matriz que contem a mediana
     for (int i =0;i<3;i++){
         mat_closed0[i] = Mat::zeros(mat_colored.rows,mat_colored.cols, CV_8UC1); // Vetor de 3 matrizes (BGR) para o grau 0
         mat_closed45[i] = Mat::zeros(mat_colored.rows,mat_colored.cols, CV_8UC1);// Vetor de 3 matrizes (BGR) para o grau 45
@@ -58,7 +104,7 @@ int main(int argc , char *argv[])
     kernel90 = Mat::zeros(13,1, CV_8UC1);
 
     //Kernel para dilatacao
-    Mat kernelD = Mat::ones(3,3, CV_8UC1);
+    Mat kernelD = Mat::ones(4,4, CV_8UC1);
     
     int testK[9] = {0,1,0,
                     1,1,1,
@@ -186,6 +232,8 @@ int main(int argc , char *argv[])
 
     Mat test = mat_hair_final.clone();
 
+    Point2d candxP = Point(0,0), candxM = Point(0,0), candyP = Point(0,0), candyM = Point(0,0);
+
     for(int y = 0 ; y < mat_hair_final.rows ; y++){
         for(int x = 0 ; x < mat_hair_final.cols ; x++){
 
@@ -196,10 +244,12 @@ int main(int argc , char *argv[])
                //Em 0º
                posX = x;
                posY = y;
+               candxP = Point(posX,posY);
                while(posX < mat_hair_final.cols){
                    posX++;
                    if(mat_hair_final.at<uchar>(Point(posX,posY)) == 0){
                        linhas.lin[0]++;
+                       candxP = Point(posX,posY);
                    } else{
                        break;
                    } 
@@ -221,10 +271,12 @@ int main(int argc , char *argv[])
                //Em 90º
                posX = x;
                posY = y;
+               candyP = Point(posX,posY);
                while(posY < mat_hair_final.rows){
                    posY++;
                    if(mat_hair_final.at<uchar>(Point(posX,posY)) == 0){
                        linhas.lin[2]++;
+                        candyP = Point(posX,posY);
                    } else{
                        break;
                    } 
@@ -245,10 +297,12 @@ int main(int argc , char *argv[])
                //Em 180º
                posX = x;
                posY = y;
+               candxM = Point(posX,posY);
                while(posX >= 0){
                    posX--;
                    if(mat_hair_final.at<uchar>(Point(posX,posY)) == 0){
                        linhas.lin[0]++;
+                       candxM = Point(posX,posY);
                    } else{
                        break;
                    } 
@@ -268,10 +322,12 @@ int main(int argc , char *argv[])
                //Em 270º
                posX = x;
                posY = y;
+               candyM = Point(posX,posY);
                while(posY >= 0){
                    posY--;
                    if(mat_hair_final.at<uchar>(Point(posX,posY)) == 0){
                        linhas.lin[2]++;
+                       candyM = Point(posX,posY);
                    } else{
                        break;
                    } 
@@ -295,15 +351,88 @@ int main(int argc , char *argv[])
                
 //               cout << x << "," << y << " / " << sortLinhas[0] << " - " << sortLinhas[1] << endl;
                if(!(sortLinhas[0] > comprimento && sortLinhas[1] < largura)){
-                mat_hair_final.at<uchar>(Point(x,y)) = 255;
+                    mat_hair_final.at<uchar>(Point(x,y)) = 255;
                }
                 
+               // INTERPOLAÇÃO
+               //dá o valor para os candidatos
+               int param = 5;
+               if(candxP.x > (mat_hair_final.cols - (param+1))){
+                    candxP.x = mat_hair_final.cols   -1;
+               } else{
+                    candxP.x += param;
+               }
+
+               if(candxM.x < param){
+                    candxM.x = 0;
+               } else{
+                    candxP.x -= param;
+               }
+
+               if(candyP.y > (mat_hair_final.rows - (param+1))){
+                    candyP.y = mat_hair_final.rows - 1;
+               } else{
+                    candyP.y += param;
+               }
+
+               if(candyM.y < param){
+                    candyM.y = 0;
+               } else{
+                    candyP.y -= param;
+               }
+
+               Point2d candFinal1, candFinal2;
+               int bgrBilinear[3] = {0,0,0};
+               //escolhe os candidatos para a interpolação
+               
+               //Se linha horizontal é maior que a linha vertical
+               //Escolhe candidatos verticais e vice versa
+               if(linhas.lin[0] > linhas.lin[2]){
+                   candFinal1 = candyP;
+                   candFinal2 = candyM;
+               } else{
+                   candFinal1 = candxP;
+                   candFinal2 = candyM;
+               }
+
+               bgrBilinear[0] = 
+                    channels[0].at<uchar>(candFinal2)*sqrt((pow(candFinal1.x-x,2)+pow(candFinal1.y-y,2))/(pow(candFinal2.x - candFinal1.x,2)+pow(candFinal2.y - candFinal1.y,2))) + 
+                    channels[0].at<uchar>(candFinal1)*sqrt((pow(candFinal2.x-x,2)+pow(candFinal2.y-y,2))/(pow(candFinal2.x - candFinal1.x,2)+pow(candFinal2.y - candFinal1.y,2)));
+
+               bgrBilinear[1] = 
+                    (int)channels[1].at<uchar>(candFinal2)*sqrt((pow(candFinal1.x-x,2)+pow(candFinal1.y-y,2))/(pow(candFinal2.x - candFinal1.x,2)+pow(candFinal2.y - candFinal1.y,2))) + 
+                    (int)channels[1].at<uchar>(candFinal1)*sqrt((pow(candFinal2.x-x,2)+pow(candFinal2.y-y,2))/(pow(candFinal2.x - candFinal1.x,2)+pow(candFinal2.y - candFinal1.y,2)));
+
+               bgrBilinear[2] = 
+                    (int)channels[2].at<uchar>(candFinal2)*sqrt((pow(candFinal1.x-x,2)+pow(candFinal1.y-y,2))/(pow(candFinal2.x - candFinal1.x,2)+pow(candFinal2.y - candFinal1.y,2))) + 
+                    (int)channels[2].at<uchar>(candFinal1)*sqrt((pow(candFinal2.x-x,2)+pow(candFinal2.y-y,2))/(pow(candFinal2.x - candFinal1.x,2)+pow(candFinal2.y - candFinal1.y,2)));
+                    
+                    //corrige valores negativos
+                    if(bgrBilinear[0] < 0){
+                        bgrBilinear[0] = (int)(channels[0].at<uchar>(candFinal1)+ channels[0].at<uchar>(candFinal2))/2;
+                        bgrBilinear[1] = (int)(channels[1].at<uchar>(candFinal1)+ channels[1].at<uchar>(candFinal2))/2;
+                        bgrBilinear[2] = (int)(channels[2].at<uchar>(candFinal1)+ channels[2].at<uchar>(candFinal2))/2;
+                    }
+                    
+                   //substitui pixels (está errado por enquanto)
+                   channels[0].at<uchar>(Point(x,y)) = bgrBilinear[0];
+                   channels[1].at<uchar>(Point(x,y)) = bgrBilinear[1];
+                   channels[2].at<uchar>(Point(x,y)) = bgrBilinear[2];
+                   //deixa o cabelo branco
+                   // channels[0].at<uchar>(Point(x,y)) = 255;
+                   // channels[1].at<uchar>(Point(x,y)) = 255;
+                   // channels[2].at<uchar>(Point(x,y)) = 255;
+
+                  //  cout << "BGR: " << (int) channels[0].at<uchar>(Point(x,y)) << " " << bgrBilinear[0] << " , " << bgrBilinear[1] << " , " << bgrBilinear[2] << endl;
+                                
             }
         }
     
     }
-    
-//    mat_hair_final = 255- mat_hair_final;
+     
+    // A matriz de cabelos que será dilatada é invertida para que a dilatação seja feita corretamente. 
+    mat_hair_final_grossa = 255 - mat_hair_final;
+
 //    dilate(mat_hair_final,mat_hair_final, kernelD);
 //    test = 255 - test;
 //    morphologyEx(mat_hair_final, mat_hair_final,cv::MORPH_OPEN, kernelD);
@@ -317,15 +446,26 @@ int main(int argc , char *argv[])
 //    mat_hair_final = 255 - mat_hair_final;
 
 
-
-    //dilate(mat_hair_final,mat_hair_final, kernelD);
-
+    // Dilata a matriz de pelos para que estes fiquem mais grossos, utilizo aqui um kernel de 4x4 com 1's
+    dilate(mat_hair_final_grossa,mat_hair_final_grossa, kernelD);
+    mat_hair_final_grossa = 255 - mat_hair_final_grossa;
 
     
+    medianFiltering(&channels[0],&channels[0],&mat_hair_final_grossa,5);
+    medianFiltering(&channels[1],&channels[1],&mat_hair_final_grossa,5);
+    medianFiltering(&channels[2],&channels[2],&mat_hair_final_grossa,5);
+    
+    Mat matFinal;
+    merge(channels,3,matFinal);
+    imwrite("imgFinal.png",matFinal);
+
+
     
 //    imshow("Imagem Original", mat_colored);
-    //imshow("Sem cabelo", mat_out);
-    imshow("Matriz Cabelos", mat_hair_final);
+//    imshow("Mediana Pelos", mat_mediana);
+    // imshow("Sem cabelo", mat_out);
+    imshow("Cabelos", mat_hair_final);
+//    imshow("Cabelos dilatados", mat_hair_final_grossa);
     waitKey(0);
     destroyAllWindows();
     imwrite("pelo1_corrigido.png", mat_hair_final);
